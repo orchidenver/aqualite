0afc4cb25fb00f7be07cb3237f3adb3d15da3fbe
Vsevolod Slavianinov <a0974377355@gmail.com>
2023-09-04 17:10:36 +0300

    ejected Form component from FormPage into separate component

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,133 @@
+import { object, z } from "zod";
+import {
+  createTsForm,
+  createUniqueFieldSchema,
+  useDescription,
+  useTsController,
+} from "@ts-react/form";
+import {
+  capitalize,
+  setCookie,
+  getCookie,
+  sendNotification,
+} from "../../utils";
+import { NameSchema, PhoneSchema, FormSchema } from "../../schemas";
+
+const TG_TOKEN = import.meta.env.VITE_TG_TOKEN;
+const CHAT_ID = import.meta.env.VITE_TG_USER_ID;
+
+const mapping = [
+  [NameSchema, MandatoryTextField] as const,
+  [PhoneSchema, MandatoryTextField] as const,
+  [z.string(), TextField] as const,
+] as const;
+
+const MyForm = createTsForm(mapping);
+
+function MandatoryTextField({ inputId }: { inputId: string }) {
+  const {
+    field: { onChange, value },
+    error,
+  } = useTsController<string>();
+  const { label, placeholder } = useDescription();
+
+  return (
+    <div className="mandatory-textfield">
+      <span>*</span>
+      <label htmlFor={inputId}>
+        {label}
+        <input
+          id={inputId}
+          type="text"
+          value={value ? value : ""}
+          placeholder={placeholder}
+          onChange={(e) => {
+            onChange(e.target.value);
+          }}
+        />
+      </label>
+      {error?.errorMessage && <span>{error?.errorMessage}</span>}
+    </div>
+  );
+}
+
+function TextField({ inputId }: { inputId: string }) {
+  const {
+    field: { onChange, value },
+    error,
+  } = useTsController<string>();
+  const { label, placeholder } = useDescription();
+
+  return (
+    <>
+      <label htmlFor={inputId}>
+        {label}
+        <input
+          id={inputId}
+          type="text"
+          value={value ? value : ""}
+          placeholder={placeholder}
+          onChange={(e) => {
+            onChange(e.target.value);
+          }}
+        />
+      </label>
+      {error?.errorMessage && <span>{error?.errorMessage}</span>}
+    </>
+  );
+}
+
+export default function Form() {
+  return (
+    <MyForm
+      onSubmit={(data) => {
+        sendNotification(JSON.stringify(data), CHAT_ID, TG_TOKEN);
+      }}
+      renderAfter={() => <input type="submit" value="Відправити" />}
+      schema={FormSchema}
+      defaultValues={{
+        name: getCookie("name"),
+        phone: getCookie("phone"),
+        street: getCookie("street"),
+        building: getCookie("building"),
+        porch: getCookie("porch"),
+        flat: getCookie("flat"),
+      }}
+      props={{
+        name: {
+          inputId: "name",
+        },
+        phone: {
+          inputId: "phone",
+        },
+        street: {
+          inputId: "street",
+        },
+        building: {
+          inputId: "building",
+        },
+        porch: {
+          inputId: "porch",
+        },
+        flat: {
+          inputId: "flat",
+        },
+      }}
+    >
+      {({ name, phone, street, building, porch, flat }) => {
+        return (
+          <>
+            {name}
+            {phone}
+            {street}
+            <div className="flex-row">
+              {building}
+              {porch}
+              {flat}
+            </div>
+          </>
+        );
+      }}
+    </MyForm>
+  );
+}
