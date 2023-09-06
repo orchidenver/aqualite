20e02dd08dea32e81ffe6ae26e6eed7d36a6891f
Vsevolod Slavianinov <a0974377355@gmail.com>
2023-09-06 21:39:14 +0300

    Added Hero section for desktop

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,15 +1,32 @@
-import * as React from "react";
+import { NavLink } from "react-router-dom";
+import { PathConstants } from "../../router/PageRoutes";
 import Footer from "../../components/Footer";
-import Card from "../../components/Card";
+import styles from "./MainPage.module.css";
+import hero from "../../assets/hero.svg";
 
 export interface IAppProps {}
 
 export default function MainPage(props: IAppProps) {
   return (
     <>
-      <div>MainPage</div>
+      <section id="hero" className={styles.hero}>
+        <div className={`${styles.container} ${styles.row}`}>
+          <div className={styles["hero__actions"]}>
+            <h1 className={styles["hero__header"]}>
+              доставка негазованої питної води
+              <p className={styles.blue}>в кривому розі</p>
+            </h1>
+            <NavLink
+              to={PathConstants.FAST_ORDER}
+              className={styles["hero__btn"]}
+            >
+              Замовити
+            </NavLink>
+          </div>
+          <img src={hero} alt="Water" className={styles["hero__img"]} />
+        </div>
+      </section>
       <Footer />
-      <Card />
     </>
   );
 }
