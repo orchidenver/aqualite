d5a10abaf1f8d588aa35636126b369686f2249f6
Vsevolod Slavianinov <a0974377355@gmail.com>
2023-09-20 17:53:36 +0300

    Fixed redirection on successful data sending.

--- src/pages/FeedbackPage/index.tsx
+++ src/pages/FeedbackPage/index.tsx
@@ -1,8 +1,11 @@
 import TransparentButton from "../../components/TransparentButton";
+import { useMediaQuery } from "../../hooks/useMediaQuery";
 import { PathConstants } from "../../router/PageRoutes";
 import styles from "./FeedBack.module.css";
 
 export default function FeedbackPage() {
+  const mobileSize = useMediaQuery("(max-width: 800px)");
+
   return (
     <section className={styles.success}>
       <div className={styles.container}>
@@ -10,7 +13,9 @@ export default function FeedbackPage() {
         <p className={styles["success__text"]}>
           Найближчим часом ми зв’яжемося з Вами для його підтвердження.
         </p>
-        <TransparentButton reference={PathConstants.FAST_ORDER}>
+        <TransparentButton
+          reference={mobileSize ? PathConstants.FAST_ORDER : "/#shop"}
+        >
           Замовити ще
         </TransparentButton>
       </div>
