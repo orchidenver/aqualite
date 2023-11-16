a6224e81438a76d673ce1b41c4e4a1da59b77fa8
Vsevolod Slavianinov <a0974377355@gmail.com>
2023-11-16 13:27:34 +0200

    Changed sizes of ProductCard and img and description in it (desktop and mobile)

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -14,28 +14,19 @@ export default function ProductCard({
   price,
 }: ProductCardInterface) {
   const { addItem, cart } = useCartContext();
-  const largeMobileSize = useMediaQuery("(min-width: 376px)");
-  const mobileSize = useMediaQuery("(max-width: 800px)");
   const location = useLocation();
-  const cardStylingSize = largeMobileSize && mobileSize;
   const product = cart.find((item) => item.id === imgLabel);
 
   return (
     <div
       className={styles.card}
-      style={{
-        maxWidth: cardStylingSize ? "65vw" : "220px",
-        maxHeight: cardStylingSize ? "50vh" : "340px",
-      }}
+      style={{ height: location.pathname === "/order" ? 450 : 400 }}
     >
       <img
         loading="lazy"
         src={img}
         alt={imgLabel}
         className={styles["card__img"]}
-        style={{
-          width: largeMobileSize && mobileSize ? "55%" : "50%",
-        }}
       />
       <h2
         className={`${styles["card__title"]} ${
